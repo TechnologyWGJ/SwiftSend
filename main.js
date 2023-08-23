dbc3f40b28eaca5771ac13f3ea00a812616d6d53
Dump <120537913+TechnologyWGJ@users.noreply.github.com>
2023-08-23 19:00:13 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,38 @@
 const { app, BrowserWindow, ipcMain, globalShortcut } = require("electron");
-const Config = require('./config.json');
 const fs = require('fs');
 const path = require('path');
+
+const userDataPath = app.getPath('userData');
+const configFilePath = path.join(userDataPath, 'config.json');
+
+const defaultConfig = {
+    ServerUrl: "http://baidu.com"
+};
+
+let Config = {};
+
+try {
+    const configData = fs.readFileSync(configFilePath, 'utf-8');
+    Config = JSON.parse(configData);
+} catch (error) {
+    console.error('Error reading or parsing config file:', error);
+    // 如果文件不存在或无法解析，创建文件并写入默认配置
+    try {
+        fs.writeFileSync(configFilePath, JSON.stringify(defaultConfig, null, 2), 'utf-8');
+        Config = defaultConfig;
+    } catch (writeError) {
+        console.error('Error creating config file:', writeError);
+        Config = {}; // 若创建文件失败，将 Config 设为一个空对象
+    }
+}
+
+function saveConfig() {
+    try {
+        fs.writeFileSync(configFilePath, JSON.stringify(Config, null, 2), 'utf-8');
+    } catch (writeError) {
+        console.error('Error saving config:', writeError);
+    }
+}
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -20,10 +51,8 @@ const createSettingsWindow = () => {
         }
     });
     ipcMain.on("Setting", (event, data) => {
-        const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-        config.ServerUrl = data;
-        const updatedConfig = JSON.stringify(config, null, 4);
-        fs.writeFileSync('config.json', updatedConfig, 'utf8');
+        Config.ServerUrl = data;
+        saveConfig();
     });
     settingsWindow.loadFile("settings.html");
 };
